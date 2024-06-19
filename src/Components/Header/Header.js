770d7fbcaa9c1799b4e335df52b5024a6cf5170c
NGSB <k971711@naver.com>
2024-06-19 18:09:11 +0900

    test

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,10 +4,20 @@ import { AiOutlineMenu, AiOutlineClose, AiOutlineUser, } from "react-icons/ai";
 import { FiSearch } from "react-icons/fi";
 
 function Header() {
+    const [clickList, setClickList] = useState(false)
+
+    function listClick(){
+        setClickList(!clickList)
+    }
+
     return(
         <div className="header_container">
             <div className="header_area">
-                <AiOutlineMenu className="header_icon"/>
+                {!clickList ? (
+                    <AiOutlineMenu className="header_icon" onClick={listClick}/>
+                ): (
+                    <AiOutlineClose className="header_icon" onClick={listClick}/>
+                )}
                 <p className="header_logo">DaydailLee</p>
                 <div className="header_icon_group">
                     <AiOutlineUser className="header_icon"/>
